997eae8f5b45e6dacf8a5ea087d8649b828af153
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-12-27 21:20:30 +0300

    add second solution task3

--- task3.py
+++ task3.py
@@ -6,15 +6,23 @@ mylist=[3,1,2,3,3,2,3,2,5,4]
 print(f'Задана последовательность чисел {mylist}')
 
 notRepeatList=[]
-repeatList=[]
-for i in mylist:
-    if i not in repeatList:
-        if i not in notRepeatList:
-            notRepeatList.append(i)
-        else:
-            repeatList.append(i)
-            notRepeatList.remove(i)
-print(f'Список повторяющихся чисел из заданной последовательности {repeatList}')
-print(f'Список неповторяющихся чисел из заданной последовательности {notRepeatList}')
+for element in mylist:
+    if mylist.count(element)==1:
+        notRepeatList.append(element)
+print(notRepeatList)
+
+# Второй вариант решения:
+
+# notRepeatList=[]
+# repeatList=[]
+# for i in mylist:
+#     if i not in repeatList:
+#         if i not in notRepeatList:
+#             notRepeatList.append(i)
+#         else:
+#             repeatList.append(i)
+#             notRepeatList.remove(i)
+# print(f'Список повторяющихся чисел из заданной последовательности {repeatList}')
+# print(f'Список неповторяющихся чисел из заданной последовательности {notRepeatList}')
 
 
