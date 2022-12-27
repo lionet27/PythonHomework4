6cf8118c85b138068dc40c7c04483a6a6117fc25
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-12-27 20:13:20 +0300

    add task3

---
+++ task3.py
@@ -0,0 +1,20 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Ввод: 3 1 2 3
+# Вывод: 2 1
+
+mylist=[3,1,2,3,3,2,3,2,5,4]
+print(f'Задана последовательность чисел {mylist}')
+
+notRepeatList=[]
+repeatList=[]
+for i in mylist:
+    if i not in repeatList:
+        if i not in notRepeatList:
+            notRepeatList.append(i)
+        else:
+            repeatList.append(i)
+            notRepeatList.remove(i)
+print(f'Список повторяющихся чисел из заданной последовательности {repeatList}')
+print(f'Список неповторяющихся чисел из заданной последовательности {notRepeatList}')
+
+
